d6fc6c9b3a403efe88ee78bb625ae1b32d3e5057
810251332@qq.com <810251332@qq.com>
2020-12-29 13:26:53 +0800

    test 2

---
+++ Week_04/FourSum.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+class FourSum {
+    class Solution {
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            
+            return new ArrayList<>();
+        }
+    }
+}
---
+++ Week_04/Jump.java
@@ -0,0 +1,7 @@
+public class Jump {
+    class Solution {
+        public int jump(int[] nums) {
+            return 0;
+        }
+    }
+}
